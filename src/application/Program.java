229a9b1ad078bfdded3ee458ce846ffe4e1ad9c1
Fabio Freire <fabiopfr@outlook.com>
2026-08-07 00:20:01 -0300

    Fix - better treatment to promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,5 @@
 package application;
 
-
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -17,7 +16,7 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
-		
+
 		while (!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
@@ -25,26 +24,33 @@ public class Program {
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
-				
+
 				boolean[][] possibleMoves = chessMatch.possibleMovies(source);
 				UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
-			
-			
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-			
-			if (capturedPiece != null) {
-				captured.add(capturedPiece);
-			}
-			}
-			catch (ChessException e) {
+
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+				if (capturedPiece != null) {
+					captured.add(capturedPiece);
+				}
+
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Entre com a peca para promocao (B/H/R/Q): ");
+					String type = sc.nextLine().toUpperCase();
+					while (!type.equals("B") && !type.equals("H") && !type.equals("R") & !type.equals("Q")) {
+						System.out.print("Valor invalido! Entre com a peca para promocao (B/H/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+					}
+					chessMatch.replacePromotedPiece(type);
+				}
+			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
-			}
-			catch (InputMismatchException e) {
+			} catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
--- src/xadrez/ChessMatch.java
+++ src/xadrez/ChessMatch.java
@@ -22,6 +22,7 @@ public class ChessMatch {
 	private boolean check;
 	private boolean checkMate;
 	private ChessPiece enPassantVulnerable;
+	private ChessPiece promoted;
 
 	private List<Piece> piecesOnTheBoard = new ArrayList<>();
 	private List<Piece> capturedPieces = new ArrayList<>();
@@ -51,6 +52,10 @@ public class ChessMatch {
 
 	public ChessPiece getEnPassantVulnerable() {
 		return enPassantVulnerable;
+	}
+
+	public ChessPiece getPromoted() {
+		return promoted;
 	}
 
 	public ChessPiece[][] getPieces() {
@@ -83,6 +88,15 @@ public class ChessMatch {
 
 		ChessPiece movedPiece = (ChessPiece) board.piece(target);
 
+		// #specialmove promotion
+		promoted = null;
+		if (movedPiece instanceof Peao) {
+			if ((movedPiece.getColor() == Color.White && target.getRow() == 0) || (movedPiece.getColor() == Color.Black && target.getRow() == 7)) {
+			promoted = (ChessPiece) board.piece(target);
+			promoted = replacePromotedPiece("Q");
+			}
+		}
+
 		check = (testCheck(opponent(currentPlayer))) ? true : false;
 
 		if (testCheckMate(opponent(currentPlayer))) {
@@ -100,6 +114,36 @@ public class ChessMatch {
 		}
 
 		return (ChessPiece) capturedPiece;
+	}
+
+	public ChessPiece replacePromotedPiece(String type) {
+		if (promoted == null) {
+			throw new IllegalStateException("Nao ha peca a ser promovida");
+		}
+		if (!type.equals("B") && !type.equals("H") && !type.equals("R") & !type.equals("Q")) {
+			return promoted;
+		}
+		Position pos = promoted.getChessPosition().toPosition();
+		Piece p = board.removePiece(pos);
+		piecesOnTheBoard.remove(p);
+
+		ChessPiece newPiece = newPiece(type, promoted.getColor());
+		board.placePiece(newPiece, pos);
+		piecesOnTheBoard.add(newPiece);
+
+		return newPiece;
+
+	}
+
+	private ChessPiece newPiece(String type, Color color) {
+		if (type.equals("B"))
+			return new Bispo(board, color);
+		if (type.equals("H"))
+			return new Cavalo(board, color);
+		if (type.equals("Q"))
+			return new Rainha(board, color);
+		return new Torre(board, color);
+
 	}
 
 	private Piece makeMove(Position source, Position target) {
